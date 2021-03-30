9d63afed3e9e0f1f198e6cc75dc3252ecb29e1ba
shanmuga raj <rajmc222@gmail.com>
2021-03-30 17:25:52 +0530

    upload source code

---
+++ TXT2HTML/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for TXT2HTML project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TXT2HTML.settings')
+
+application = get_asgi_application()
---
+++ TXT2HTML/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for TXT2HTML project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TXT2HTML.settings')
+
+application = get_wsgi_application()
